8c443537cfe37440c35254fa7c6635634487f4ee
Konstantin <fandeluk.k@gmail.com>
2020-07-16 19:48:49 +0300

    На проверку

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Button from "./components/Button";
 import Import from "./components/Input";
+import ListItem from "./components/ListItem";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [listItem, setListItem] = useState([]);
+
+  const handlerAddList = () => {
+    let itemsArray = localStorage.getItem("todo") ? JSON.parse(localStorage.getItem("todo")) : [];
+
+    itemsArray.push(inputValue);
+
+    setListItem([...inputValue, ...listItem]);
+    localStorage.setItem("todo", JSON.stringify(itemsArray));
+
+    console.log("W");
+  };
+
+  const handlerInputValue = (event) => {
+    let str = event.target.closest("input").value;
+    setInputValue(str);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <Import onChange={(event) => {}} />
-        <Button name="ADD Task" />
+        <Import
+          onChange={(event) => {
+            handlerInputValue(event);
+          }}
+        />
+        <Button
+          name="ADD Task"
+          onClick={(event) => {
+            handlerAddList();
+          }}
+        />
+        <ListItem />
       </header>
     </div>
   );
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,7 +2,27 @@ import React from "react";
 import styled from "styled-components";
 
 function ListItem(props) {
-  return <></>;
+  const arrTest = ["Первый", "Второй", "Третий", "Четвертый", "Пятый"];
+  let itemsArray = localStorage.getItem("todo") ? JSON.parse(localStorage.getItem("todo")) : [];
+  const List = styled.ul`
+    list-style-type: decimal;
+  `;
+
+  const inputElement = itemsArray.map((item) => {
+    return (
+      <>
+        <li>{item}</li>
+        <button>Done Task</button>
+        <button>Del Task</button>
+      </>
+    );
+  });
+
+  return (
+    <>
+      <List>{inputElement}</List>
+    </>
+  );
 }
 
 export default ListItem;
